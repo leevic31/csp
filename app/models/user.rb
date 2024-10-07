7fb65c2b75b7702b7c1e62b5ea15adc1b1633f84
leevic31 <leevic31@gmail.com>
2024-10-07 19:44:57 -0400

    Added Admin, User and Guest roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLES = %w[admin user guest]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= 'user'
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
+  def user?
+    role == 'user'
+  end
+
+  def guest?
+    role == 'guest'
+  end
 end
