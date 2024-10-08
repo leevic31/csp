9fd01c0a4412511daefcca12435d65b52d37873f
leevic31 <leevic31@gmail.com>
2024-10-07 20:14:15 -0400

    Implemented File Uploads

---
+++ app/controllers/file_uploads_controller.rb
@@ -0,0 +1,46 @@
+class FileUploadsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_file_upload, only: [:destroy]
+  
+    def index
+        if current_user.admin?
+            @file_uploads = FileUpload.all
+        else
+            @file_uploads = current_user.file_uploads
+        end
+    end
+  
+    def create
+        if current_user.user?
+            @file_upload = current_user.file_uploads.new(file_upload_params)
+
+            if @file_upload.save
+                redirect_to file_uploads_path, notice: "File uploaded successfully."
+            else
+                render :index
+            end
+        else
+            redirect_to file_uploads_path, alert: "Not authorized to upload this file"
+        end
+
+    end
+  
+    def destroy
+        if current_user.user? && @file_upload.user == current_user 
+            @file_upload.destroy
+            redirect_to file_uploads_path, notice: "File deleted successfully."
+        else
+            redirect_to file_uploads_path, alert: "Not authorized to delete this file"
+        end
+    end
+  
+    private
+  
+    def set_file_upload
+      @file_upload = current_user.file_uploads.find(params[:id])
+    end
+  
+    def file_upload_params
+      params.require(:file_upload).permit(:file, :name)
+    end
+end
