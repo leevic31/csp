dae21d42a0bea96f69e15102aaeee72b2ae03f99
leevic31 <leevic31@gmail.com>
2024-10-08 21:05:21 -0400

    Added accounts to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create(email: "admin@example.com", password: "password", role: "admin")
+User.create(email: "user@example.com", password: "password", role: "user")
+User.create(email: "guest@example.com", password: "password", role: "guest")
+
+puts "Users created successfully!"
